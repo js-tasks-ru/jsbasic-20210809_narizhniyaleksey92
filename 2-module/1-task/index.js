5b7c355bba68dc32c29056f17674d90a4df902bf
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-08-17 09:48:43 +0300

    Solution of problem 1 of the second lesson

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
-  // ваш код...
+ let sumSalaries = 0;
+
+  for( let key in salaries) {
+      if( typeof salaries[key] == "number" && !isNaN(salaries[key]) && isFinite(salaries[key]) ) {
+          sumSalaries  += salaries[key];
+      }
+  }
+
+  return sumSalaries;
 }
