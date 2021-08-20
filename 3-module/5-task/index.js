660305ad32d328cba01676ea36c23688567e4af3
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-08-20 13:22:47 +0300

    Solution of problem 5 of the third lesson

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,9 @@
 function getMinMax(str) {
-  // ваш код...
+  let arr = str.split(' ').filter( a => !isNaN(+a) && isFinite(+a) && typeof +a === 'number' ? +a:false);
+  let obj = {};
+
+  obj.min = Math.min(...arr);
+  obj.max = Math.max(...arr);
+
+  return obj;
 }
