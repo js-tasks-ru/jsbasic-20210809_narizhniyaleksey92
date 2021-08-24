eb9d987adea8420b52e63191114897b2cbe43bb4
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-08-24 11:20:48 +0300

    Solution of problem 3 of the fourth lesson

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,21 @@
 function highlight(table) {
-  // ваш код...
+    let rows = table.tBodies[0].rows;
+    
+    for(let i = 0; i < rows.length; i++) {
+        let row = rows[i];
+        
+        if(row.cells[1].textContent < 18) {
+            row.style['text-decoration'] = 'line-through';}
+
+        row.classList.add( row.cells[2].textContent === 'm' ? 'male' : 'female');
+
+        if(row.cells[3].hasAttribute('data-available')){
+            row.classList
+               .add( row.cells[3].getAttribute('data-available') === 'true' ? 'available' : 'unavailable');
+        }else{
+            row.hidden = true;
+        }
+
+    }
+	
 }
