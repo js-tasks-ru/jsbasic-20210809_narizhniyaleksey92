056e71b384de0b65109a8de1ea01da71d2bc72b6
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-09-03 17:46:35 +0300

    Solution of problem 1 of the sixth lesson

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -11,8 +11,31 @@
  *          city: 'Petrozavodsk'
  *      }
  *
- */
+//  */
 export default class UserTable {
   constructor(rows) {
+    this.rows = rows;
+    this.elem = this.render();
+  }
+
+  deleteRow(event) {
+    if (event.target.tagName != 'BUTTON') {return;}
+
+    let target = event.target.closest('tr');
+    target.remove();
+  }
+
+  render() {
+    let table = document.createElement('table');
+
+    let tableHead = "<thead><tr><th>Имя</th><th>Возраст</th><th>Зарплата</th><th>Город</th><th></th></tr></thead>";
+    table.innerHTML = tableHead;
+
+    let tableBody = this.rows.map(val => { return `<tr><td>${val.name}</td><td>${val.age}</td><td>${val.salary}</td><td>${val.city}</td><td><button>X</button></td></tr>`; }).join('');
+    table.innerHTML += `<tbody>${tableBody}</tbody>`;
+
+    table.addEventListener('click', this.deleteRow);
+
+    return table;
   }
 }
