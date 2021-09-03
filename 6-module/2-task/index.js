c86e67d3178a9073585a675741a4b1e5870f655a
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-09-03 18:51:53 +0300

    Solution of problem 2 of the sixth lesson

--- 6-module/2-task/index.js
+++ 6-module/2-task/index.js
@@ -1,4 +1,52 @@
+// export default class ProductCard {
+//   constructor(product) {
+//   }
+// }
+
+import createElement from '../../assets/lib/create-element.js';
+
 export default class ProductCard {
+
   constructor(product) {
+    this.name = product.name;
+    this.price = product.price;
+    this.category = product.category;
+    this.image = product.image;
+    this.id = product.id;
+    this.elem = this.render();
   }
+
+  render() {
+    this.card = createElement(`
+        <div class="card">
+          <div class="card__top">
+              <img src="/assets/images/products/${this.image}" class="card__image" alt="product">
+              <span class="card__price">€${this.price.toFixed(2)}</span>
+          </div>
+          <div class="card__body">
+              <div class="card__title">${this.name}</div>
+              <button type="button" class="card__button">
+                  <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+              </button>
+          </div>
+        </div>
+      `);
+
+    this.card.addEventListener('click', this.onClick);
+
+    return this.card;
+  }
+
+  onClick = (event) => {
+
+    if (!event.target.closest('.card__button')) { return;}
+
+    const ev = new CustomEvent('product-add', {
+      detail: this.id, 
+      bubbles: true});
+
+    this.card.dispatchEvent(ev);
+  }
+
 }
+
