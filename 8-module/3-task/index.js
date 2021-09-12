6e786786ade9e8a0afb995459417546943594f1f
Aleksey Narizhniy <narizhniyaleksey92@gmail.com>
2021-09-12 13:48:47 +0300

    Solution of problem 3 of the eighth lesson

--- 8-module/3-task/index.js
+++ 8-module/3-task/index.js
@@ -6,29 +6,62 @@ export default class Cart {
   }
 
   addProduct(product) {
-    // ваш код
+
+    if(typeof product === 'undefined' || product === null ) {
+      return;
+      }
+
+    let findMessage = false;
+    for(let item of this.cartItems){
+
+      if(item.product.id == product.id){
+      item.count += 1;
+      findMessage = true;
+      break;
+      }
+
+    }
+
+    if(findMessage == false) this.cartItems.push({product:product, count:1});
+
+    let cartItem = this.cartItems.slice();
+    this.onProductUpdate(cartItem);
   }
 
   updateProductCount(productId, amount) {
-    // ваш код
+
+    for(let i = 0; i < this.cartItems.length; i++){
+
+      if(this.cartItems[i].product.id == productId){
+        this.cartItems[i].count += amount;
+          if(this.cartItems[i].count == 0 ) this.cartItems.splice(i, 1);
+        break;
+      }
+
+    }
+
+   this.onProductUpdate( this.cartItems);
   }
 
   isEmpty() {
-    // ваш код
+   return this.cartItems.length <= 0 ? true : false;
   }
 
   getTotalCount() {
-    // ваш код
+   let counter = 0;
+   Array.from(this.cartItems).forEach(val => counter += val.count)
+   return counter;
   }
 
   getTotalPrice() {
-    // ваш код
+    let totalPrice = Array.from(this.cartItems).reduce(function(previousValue, currentValue, index, array) {
+       return previousValue + (currentValue.product.price * currentValue.count);
+      }, 0);
+    return totalPrice;
   }
 
   onProductUpdate(cartItem) {
     // реализуем в следующей задаче
-
     this.cartIcon.update(this);
   }
 }
-
